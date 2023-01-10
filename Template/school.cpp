44603fd5ec311ece7117f93e82955f210154a942
azhan3 <96319134+azhan3@users.noreply.github.com>
2023-01-10 11:57:31 -0500

    Create school.cpp

---
+++ Template/school.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <class T>
+using vec = vector<T>;
+
+#define FOR(i, s, e) for (ll i = (ll)s; i < (ll)e; i++)
+#define dbg(x) cerr << "ln" << __LINE__ << ": " << #x << " = " << x << endl
+
+void solve() {
+
+}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  solve();
+  return 0;
+}
